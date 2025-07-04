aff07c4d79a43833b0c4e3c2516df86a3adfda5a
MSI\User <lisa051920@gmail.com>
2025-07-04 13:55:12 +0800

    【】 測試api

--- web_api/webapi_order_system/obj/Debug/net8.0/webapi_order_system.AssemblyInfo.cs
+++ web_api/webapi_order_system/obj/Debug/net8.0/webapi_order_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi_order_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0c15833d222619ae74cc25be20bc69e11dd1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce43f3d00d382874a442ca7dccfa3c87edbb9ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi_order_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi_order_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
