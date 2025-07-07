df4fa865c85d3900d8efb6150818f40a7f9f451b
MSI\User <lisa051920@gmail.com>
2025-07-07 13:57:12 +0800

    【】 測試api

--- web_api/webapi_order_system/Program.cs
+++ web_api/webapi_order_system/Program.cs
@@ -33,51 +33,51 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-var app = builder.Build();
-
-
-//^_^ 20250701 add by lisa for 啟用 CORS ==S==
-app.Use(async (context, next) =>
-{
-    context.Response.Headers.Add("Access-Control-Allow-Origin", "https://ruru-wen519.github.io");
-    await next();
-});
-// 啟用 CORS
-app.UseCors("AllowMyWebPage");
-//^_^ 20250701 add by lisa for 啟用 CORS ==E==
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-//app.MapControllers();//^_^ 20250701 mark by lisa 
-
-//^_^ 20250701 add by lisa for 啟用 CORS ==S==
-// 新增路由配置
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapPost("/api/orders", async (context) =>
-{
-    // 處理 POST 請求的邏輯
-    var order = await context.Request.ReadFromJsonAsync<Order>();
-    // ...
-    await context.Response.WriteAsync("Order received");
-});
-//^_^ 20250701 add by lisa for 啟用 CORS ==E==
-
 try
 {
-    app.Run();
+	var app = builder.Build();
+
+
+	//^_^ 20250701 add by lisa for 啟用 CORS ==S==
+	app.Use(async (context, next) =>
+	{
+		context.Response.Headers.Add("Access-Control-Allow-Origin", "https://ruru-wen519.github.io");
+		await next();
+	});
+	// 啟用 CORS
+	app.UseCors("AllowMyWebPage");
+	//^_^ 20250701 add by lisa for 啟用 CORS ==E==
+
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	//app.MapControllers();//^_^ 20250701 mark by lisa 
+
+	//^_^ 20250701 add by lisa for 啟用 CORS ==S==
+	// 新增路由配置
+	app.MapControllerRoute(
+		name: "default",
+		pattern: "{controller=Home}/{action=Index}/{id?}");
+
+	app.MapPost("/api/orders", async (context) =>
+	{
+		// 處理 POST 請求的邏輯
+		var order = await context.Request.ReadFromJsonAsync<Order>();
+		// ...
+		await context.Response.WriteAsync("Order received");
+	});
+	//^_^ 20250701 add by lisa for 啟用 CORS ==E==
+
+
+		app.Run();
 }
 catch (Exception ex)
 {
