30c587a5d9dce638593f6f268c2355d87f841236
MSI\User <lisa051920@gmail.com>
2025-07-08 16:19:43 +0800

    【】 處理衝突

--- web_api/webapi_order_system/Program.cs
+++ web_api/webapi_order_system/Program.cs
@@ -1,5 +1,3 @@
-using webapi_order_system.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 //^_^ 20250701 add by lisa for �ҥ� CORS ==S==
@@ -14,17 +12,6 @@ builder.Services.AddCors(options =>
     });
 });
 //^_^ 20250701 add by lisa for �ҥ� CORS ==E==
-//^_^ 20250701 add by lisa for �t�m HTTPS �ݤf ==S==
-// �t�m HTTPS �ݤf
-//builder.WebHost.UseKestrel(options =>
-//{
-//    options.ListenLocalhost(5000);
-//    options.ListenLocalhost(5001, listenOptions =>
-//    {
-//        listenOptions.UseHttps();
-//    });
-//});
-//^_^ 20250701 add by lisa for �t�m HTTPS �ݤf ==E==
 
 // Add services to the container.
 
@@ -33,68 +20,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-try
+var app = builder.Build();
+
+//^_^ 20250701 add by lisa for �ҥ� CORS ==S==
+app.Use(async (context, next) =>
 {
-	var app = builder.Build();
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "https://web-front-code.onrender.com");
+    await next();
+});
+// �ҥ� CORS
+app.UseCors("AllowMyWebPage");
+//^_^ 20250701 add by lisa for �ҥ� CORS ==E==
 
-	//^_^ 20250701 add by lisa for �ҥ� CORS ==S==
-	app.Use(async (context, next) =>
-	{
-		context.Response.Headers.Add("Access-Control-Allow-Origin", "https://web-front-code.onrender.com");
-		await next();
-	});
-	// �ҥ� CORS
-	app.UseCors("AllowMyWebPage");
-	//^_^ 20250701 add by lisa for �ҥ� CORS ==E==
-
-	// Configure the HTTP request pipeline.
-	if (app.Environment.IsDevelopment())
-	{
-		app.UseSwagger();
-		app.UseSwaggerUI();
-        //app.UseHttpsRedirection(); //lisa
-    }
-    //lisa ==S==
-    //app.UseRouting();
-    //app.UseEndpoints(endpoints =>
-    //{
-    //    endpoints.MapControllers();
-    //});
-    //lisa ==E==
-    app.UseHttpsRedirection();
-
-	app.UseAuthorization();
-
-	//app.MapControllers();//^_^ 20250701 mark by lisa 
-
-	//^_^ 20250701 add by lisa for �ҥ� CORS ==S==
-	// �s�W���Ѱt�m
-	app.MapControllerRoute(
-		name: "default",
-		pattern: "{controller=Home}/{action=Index}/{id?}");
-
-	app.MapPost("/api/orders", async (context) =>
-	{
-		// �B�z POST �ШD���޿�
-		var order = await context.Request.ReadFromJsonAsync<Order>();
-		// ...
-		await context.Response.WriteAsync("Order received");
-	});
-	//^_^ 20250701 add by lisa for �ҥ� CORS ==E==
-
-
-	app.Run();
-}
-catch (Exception ex)
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    // �O�����`��T
-    Console.WriteLine($"An error occurred: {ex.Message}");
-    Console.WriteLine(ex.StackTrace);
-
-    // �i�H�b�o�̲K�[��L���`�B�z�޿�,�Ҧp:
-    // - ��^���~�T�����Ȥ��
-    // - �q���޲z��
-    // - �����L���~�B�z�ʧ@
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
 
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- web_api/webapi_order_system/obj/Debug/net8.0/webapi_order_system.AssemblyInfo.cs
+++ web_api/webapi_order_system/obj/Debug/net8.0/webapi_order_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi_order_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c38fa6388d14d57f191b95e3967588758e09ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c67ade36bf3ba50b2017b1203a124ce2e7900d")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi_order_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi_order_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
