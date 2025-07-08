8d90f1dcab496e9c8c2554c0c55e5e57bf45461e
MSI\User <lisa051920@gmail.com>
2025-07-08 14:55:33 +0800

    【】 測試資料庫

--- web_api/webapi_order_system/Controllers/WeatherForecastController.cs
+++ web_api/webapi_order_system/Controllers/WeatherForecastController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
 using webapi_order_system.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace webapi_order_system.Controllers
 {
@@ -19,10 +23,28 @@ namespace webapi_order_system.Controllers
         //    };
         //    return Ok(response);
         //}
+
         public ResponseModel login([FromBody] RequestModel request)
         {
             ResponseModel response = new ResponseModel();
-            if ("2025".Equals(request.account_input))
+
+            DataTable dataTable = new DataTable();
+
+            // 讀取連接字串
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // 撈取 test01 資料表
+                string query = "SELECT * FROM [test01] where username = '" + request.account_input + "' and userpassword = '" + request.password_input + "";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataTable);
+            }
+
+            if (dataTable.Rows.Count > 0)
             {
                 response.is_correct = "0";
             }
@@ -30,8 +52,23 @@ namespace webapi_order_system.Controllers
             {
                 response.is_correct = "1";
             }
-            response.message = "";
+            //if ("2025".Equals(request.account_input))
+            //{
+            //    response.is_correct = "0";
+            //}
+            //else
+            //{
+            //    response.is_correct = "1";
+            //}
+                response.message = "";
             return response;
+        }
+
+        private readonly IConfiguration _configuration;
+
+        public DataController(IConfiguration configuration)
+        {
+            _configuration = configuration;
         }
     }
 }
