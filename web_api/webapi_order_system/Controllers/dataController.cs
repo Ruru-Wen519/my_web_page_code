f9d4609fc56405d060864710483ff80e198f3208
MSI\User <lisa051920@gmail.com>
2025-07-14 14:27:44 +0800

    【】 測試資料庫

--- web_api/webapi_order_system/Controllers/dataController.cs
+++ web_api/webapi_order_system/Controllers/dataController.cs
@@ -59,24 +59,16 @@ namespace webapi_order_system.Controllers
         {
             ResponseDto response = new ResponseDto();
 
-            //DataTable dataTable = new DataTable();
+            DataTable dataTable = new DataTable();
 
-            // 執行原生 SQL 查詢並將結果轉換為 DataTable
-            //string query = "SELECT * FROM [login_data] WHERE login_name = '@login_name' AND login_password = '@login_password'";
-            //var parameters = new[] { new SqlParameter("@login_name", request.account_input), new SqlParameter("@login_password", request.password_input) };
-            string query = "SELECT * FROM [login_data] WHERE login_name = '" + request.account_input + "' AND login_password = '" + request.password_input + "'";
+            //// 執行原生 SQL 查詢並將結果轉換為 DataTable
+            ////string query = "SELECT * FROM [login_data] WHERE login_name = '@login_name' AND login_password = '@login_password'";
+            ////var parameters = new[] { new SqlParameter("@login_name", request.account_input), new SqlParameter("@login_password", request.password_input) };
+            //string query = "SELECT * FROM [login_data] WHERE login_name = '" + request.account_input + "' AND login_password = '" + request.password_input + "'";
 
-            var dto_Login_Datas = _web_DesignContext.Set<login_data>().FromSqlRaw(query).ToList();
+            //var dto_Login_Datas = _web_DesignContext.Set<login_data>().FromSqlRaw(query).ToList();
 
-            if (dto_Login_Datas.Count > 0)
-            {
-                response.is_correct = "0";
-            }
-            else
-            {
-                response.is_correct = "1";
-            }
-            //if ("2025".Equals(request.account_input))
+            //if (dto_Login_Datas.Count > 0)
             //{
             //    response.is_correct = "0";
             //}
@@ -84,6 +76,45 @@ namespace webapi_order_system.Controllers
             //{
             //    response.is_correct = "1";
             //}
+            ////if ("2025".Equals(request.account_input))
+            ////{
+            ////    response.is_correct = "0";
+            ////}
+            ////else
+            ////{
+            ////    response.is_correct = "1";
+            ////}
+
+            try
+            {
+                var cb = new SqlConnectionStringBuilder();
+                cb.DataSource = "tcp:web-design-server.database.windows.net";
+                cb.UserID = "Web";
+                cb.Password = "bew_0519";
+                cb.InitialCatalog = "web_design";
+
+                using (var connection = new SqlConnection(cb.ConnectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM [login_data] WHERE login_name = '" + request.account_input + "' AND login_password = '" + request.password_input + "'";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable);
+
+                }
+                if (dataTable.Rows.Count > 0)
+                {
+                    response.is_correct = "0";
+                }
+                else
+                {
+                    response.is_correct = "1";
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
             response.message = "";
             return response;
         }
